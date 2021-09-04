cc06e799d1a2c7ade488b49f44fcc9ab18915694
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2021-09-04 23:16:24 +0430

    Swagger: Meta added for main description

--- fastestMirrorFinder/main.go
+++ fastestMirrorFinder/main.go
@@ -1,3 +1,26 @@
+// Debian fastest mirror finder
+//
+// The purpose of this application is to:
+//
+// 1) First scrap all mirrors from Debian site
+//
+// 2) Find the fastest among them
+//
+// 3) Present the answer using REST api
+//
+//     Schemes: http
+//     Host: localhost:8080
+//     BasePath: /
+//     Version: 0.0.1
+//     License: GPL V3 https://github.com/farbodahm/lets-go/blob/main/LICENSE
+//     Contact: Farbod Ahmadian<farbodahmadian2014@gmail.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+// swagger:meta
 package main
 
 import (
