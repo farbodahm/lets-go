87aa34c492d68961e4344467c7a8b37915c1d960
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2021-09-27 17:08:55 +0330

    Tests: Added simple Monkey lang syntax

--- monkeyLangInterpreter/lexer/lexer_test.go
+++ monkeyLangInterpreter/lexer/lexer_test.go
@@ -41,3 +41,71 @@ func TestNextTokenDelimiters(t *testing.T) {
 		}
 	}
 }
+
+func TestNextTokenSimpleLangStructure(t *testing.T) {
+	input := "let five = 5;\n" +
+		"let ten = 10;\n\n" +
+		"let add = fn(x, y) {\n" +
+		"x + y;\n" +
+		"};\n\n" +
+		"let result = add(five, ten);\n"
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	lexer := lexer.NewLexer(input)
+
+	for i, test := range tests {
+		nextToken := lexer.NextToken()
+
+		if nextToken.Type != test.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q, literal=%q",
+				i, test.expectedType, nextToken.Type, nextToken.Literal)
+		}
+
+		if nextToken.Literal != test.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, test.expectedLiteral, nextToken.Literal)
+		}
+	}
+}
