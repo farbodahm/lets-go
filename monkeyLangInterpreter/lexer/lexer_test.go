4ee5e8614e39e22753d04917a0f2338d89d4e43c
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2021-10-03 21:32:02 +0330

    Tests: Added for 2 char operators: ==, !=

--- monkeyLangInterpreter/lexer/lexer_test.go
+++ monkeyLangInterpreter/lexer/lexer_test.go
@@ -57,6 +57,9 @@ func TestNextTokenSimpleLangStructure(t *testing.T) {
 		} else {
 			return false;
 		}
+
+		10 == 10;
+		10 != 9;
 		`
 
 	tests := []struct {
@@ -128,6 +131,14 @@ func TestNextTokenSimpleLangStructure(t *testing.T) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
